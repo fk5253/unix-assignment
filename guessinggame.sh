6528b453d63e4c49321eca9df827e15a4a20ea45
fk5253 <58441071+fk5253@users.noreply.github.com>
2020-05-21 23:57:09 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -15,14 +15,14 @@ function guess_filenum {
 	else
 		if [[ $guess -gt $filenum ]]
 		then
-			echo "There is less... try again and press Enter :"
+			echo "You have guessed more... try again with less number and press Enter :"
 			guess_filenum
 		else
-			echo "There is more... try again and press Enter :"
+			echo "You have guessed less... try again with greater number and press Enter :"
 			guess_filenum
 		fi
 	fi
 }
 echo "Welcome to the guessing game!"
-echo "Guess how many files are in the current directory (pretend you don't know) and press Enter :"
+echo "Guess how many files are in the current directory and press Enter :"
 guess_filenum
