b16325afccfb19396ffc7306797e6b12027eb670
fk5253 <58441071+fk5253@users.noreply.github.com>
2020-09-01 18:22:23 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-filenum=$(ls | wc -l)
+filenum=$(ls -l |grep "^-"|wc -l)
 function guess_num {
-	read guess 
- while[[$guess -ne $filenum]]
+ read guess 
+ while [ $guess -ne $filenum ]
  do
-		if [[ $guess -gt $filenum ]]
+		if [ $guess -gt $filenum ]
 		then
 			echo "You have guessed more... try again with less number and press Enter :"
 			guess_num
@@ -13,9 +13,9 @@ function guess_num {
 			guess_num
 		fi
  done
- echo "wow..!! you guessed it right."
- echo "Hope you enjoyed it."
 }
 echo "Welcome to the guessing game!!..Hope u like it."
 echo "Guess how many files are in the current directory and press Enter :"
 guess_num
+echo "wow..!! you guessed it right."
+echo "Hope you enjoyed it."
